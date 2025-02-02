5c0d4e1f9a086dadf347fce06af1368dff2e9b25
Yilmer07 <83854401+Yilmer07@users.noreply.github.com>
2025-02-02 02:47:39 -0500

    Add files via upload
    Init commit

---
+++ controller/envioMail.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../controller/PHPMailer/Exception.php';
+require '../controller/PHPMailer/PHPMailer.php';
+require '../controller/PHPMailer/SMTP.php';
+
+//El contact debe tener la propiedad name, sino generara error
+$nombre =  'Diana';
+$email =   'yilmer.chaponan93@gmail.com';
+$mensaje = 'La carta fue leída.';
+
+/*Cuerpo del Correo*/
+$contenido = <<< HTML
+    <h1><strong>Te notificamos que</strong></h1>
+    <p><strong>Mensaje:</strong> $mensaje.</p>
+HTML;
+
+$headers = "MIME-Version: 1.0 \r\n";
+$headers .= "Content-type: text/html; \r\n";
+//Create an instance; passing `true` enables exceptions
+$mailer = new PHPMailer(true);
+
+try {
+    //Recipients
+    $mailer->setFrom($email, $nombre);// ¿De quein llegara?
+    $mailer->addAddress('yilmer.chaponan93@gmail.com', 'Yilmer Ch');
+    //Content
+    $mailer->isHTML(true);                                  //Set email format to HTML
+    $mailer->Subject = 'Carta Diana';
+    //$mailer->Body    = $contenido;
+    $mailer->CharSet = 'UTF-8';
+    $mailer->msgHTML($contenido);
+    $mailer->altBody = strip_tags($contenido);
+    //
+    $mailer->send();
+        echo json_encode('success');
+    } catch (Exception $e) {
+        echo json_encode("El mensaje no pudo ser enviado: Error: {$mailer->ErrorInfo}");
+    }
+
+?>
---
+++ scripts.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    console.log('INICIANDO');
+    // Detectar cuando la página se abre
+    sendEmailNotification();
+
+    //
+    async function sendEmailNotification(){
+
+        const response = await fetch(
+            '/controller/envioMail.php',
+            {
+                method : 'POST'
+            }
+        );
+        const data = await response.json();
+        console.log('datas',data);
+        if(data === "success"){
+          console.log("El mensaje fue enviado correctamente");
+        }else{
+          console.log('Ocurrio un error');
+        }
+    }
+};
