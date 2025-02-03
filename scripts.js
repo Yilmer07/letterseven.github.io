e6b20de868424cd98c4dd5843aabf927aeb080e9
Yilmer Chaponan <developer.yilmer@gmail.com>
2025-02-02 21:18:42 -0500

    error email

--- scripts.js
+++ scripts.js
@@ -2,25 +2,25 @@ window.onload = function() {
     console.log('INICIANDO');
     // Detectar cuando la página se abre
     sendEmailNotification();
-
-    //
-    async function sendEmailNotification(){
-      let dataContact = new FormData();
-      dataContact.append('nombre', 'Diana');
-      dataContact.append('property', 'value');
-        const response = await fetch(
-            'controller/envioMail.php',
-            {
-                method : 'POST',
-                body : dataContact
-            }
-        );
-        const data = await response.json();
-        console.log('datas',data);
-        if(data === "success"){
-          console.log("El mensaje fue enviado correctamente");
-        }else{
-          console.log('Ocurrio un error');
-        }
-    }
 };
+
+ //
+ async function sendEmailNotification(){
+  let dataContact = new FormData();
+  dataContact.append('nombre', 'Diana');
+  dataContact.append('property', 'value');
+    const response = await fetch(
+        'controller/envioMail.php',
+        {
+            method : 'POST',
+            body : dataContact
+        }
+    );
+    const data = await response.json();
+    console.log('datas',data);
+    if(data === "success"){
+      console.log("El mensaje fue enviado correctamente");
+    }else{
+      console.log('Ocurrio un error');
+    }
+}
